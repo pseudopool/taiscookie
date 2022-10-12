c54dc3f6d5ee8b62dc0e1afd4160e61e200fcc07
tai song <pseudopool@gmail.com>
2022-10-12 19:02:14 +0900

    add: footer에 github 링크 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,27 @@
 import { css } from "@emotion/react";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
 
 export default function Footer() {
-  const footer = css`
-    text-align: center;
-    width: 100%;
-    bottom: 0px;
-    padding: 1rem;
-    font-size: 0.9rem;
-    color: #b4b4b4;
-  `;
-  return <footer css={footer}>@타이의 쿠키 by Tai Song </footer>;
+  return (
+    <footer css={footer}>
+      <Link href="https://github.com/pseudopool">
+        <Icon icon="akar-icons:github-fill" className="icon" />
+      </Link>
+      <p>@타이의 쿠키 by Tai Song</p>
+    </footer>
+  );
 }
+
+const footer = css`
+  text-align: center;
+  width: 100%;
+  bottom: 0px;
+  padding: 1rem;
+  font-size: 0.9rem;
+  color: #b4b4b4;
+  .icon {
+    cursor: pointer;
+    font-size: 1.5rem;
+  }
+`;
