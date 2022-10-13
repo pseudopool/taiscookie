b3e2d2cda8bfadd0844b82bce13b7a81bcbbb08c
tai song <pseudopool@gmail.com>
2022-10-13 23:45:58 +0900

    chore: posts 페이지 css 수정

--- src/components/posts/all-posts.tsx
+++ src/components/posts/all-posts.tsx
@@ -1,31 +1,18 @@
 import PostPreview from "./post-preview";
 import type Post from "../../interfaces/post";
 import { css } from "@emotion/react";
+import { Theme } from "../../styles/Theme";
 
 type Props = {
   posts: Post[];
 };
 
-const allposts = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  h2 {
-    text-align: center;
-    font-weight: 300;
-    font-size: 1.2rem;
-  }
-  ul {
-    max-width: 760px;
-    width: 100%;
-    padding: 0 1rem;
-  }
-`;
-
 const AllPosts = ({ posts }: Props) => {
   return (
     <section css={allposts}>
-      <h2>{posts.length} posts</h2>
+      <h2>
+        <span>{posts.length}</span> posts
+      </h2>
       <ul
         css={css`
           padding: 0;
@@ -47,3 +34,24 @@ const AllPosts = ({ posts }: Props) => {
 };
 
 export default AllPosts;
+
+const allposts = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h2 {
+    font-family: "Courier New", Courier, monospace;
+    text-align: center;
+    font-weight: 300;
+    font-size: 1.2rem;
+  }
+  ul {
+    max-width: 760px;
+    width: 100%;
+    padding: 0 1rem;
+  }
+  span {
+    font-weight: 500;
+    color: ${Theme.mainFontColor};
+  }
+`;
