f0c2f4611ef169f27e873b39886298b82d7ffc24
tai song <pseudopool@gmail.com>
2022-10-19 17:07:24 +0900

    feat: 포트폴리오 pdf 링크 추가

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,34 +1,16 @@
 import { css } from "@emotion/react";
 import Head from "next/head";
 import { projects } from "../../public/assets/projects/projects";
+import PdfLink from "../components/projects/portfolio-link";
 import Project from "../components/projects/project-preview";
 
 export default function Portfolio() {
-  const portfolio = css`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    div {
-      padding-inline: 3rem;
-      margin-top: 1.5rem;
-      max-width: 1000px;
-      display: grid;
-      grid-template-columns: 30% 30% 30%;
-      place-content: center;
-      grid-gap: 1.5rem;
-    }
-    @media screen and (max-width: 768px) {
-      div {
-        grid-template-columns: 100%;
-      }
-    }
-  `;
-
   return (
     <>
       <Head>
         <title>포트폴리오 | 타이의 쿠키</title>
       </Head>
+      <PdfLink />
       <section css={portfolio}>
         <div>
           {projects
@@ -42,3 +24,23 @@ export default function Portfolio() {
     </>
   );
 }
+
+const portfolio = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div {
+    padding-inline: 3rem;
+    margin-top: 1.5rem;
+    max-width: 1000px;
+    display: grid;
+    grid-template-columns: 30% 30% 30%;
+    place-content: center;
+    grid-gap: 1.5rem;
+  }
+  @media screen and (max-width: 768px) {
+    div {
+      grid-template-columns: 100%;
+    }
+  }
+`;
