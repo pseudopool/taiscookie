6fe647cfe801ee1be3a868751a3d2f3b8ad7ad66
tai song <pseudopool@gmail.com>
2023-04-23 21:27:25 +0900

    fix typo

--- src/components/posts/all-posts.tsx
+++ src/components/posts/all-posts.tsx
@@ -1,5 +1,5 @@
 import PostPreview from "./post-preview";
-import type Post from "interfaces/post";
+import type { Post } from "interfaces/post";
 import { css } from "@emotion/react";
 import { Theme } from "styles/Theme";
 
