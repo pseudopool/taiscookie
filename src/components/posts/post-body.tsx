d4e8e54ed7bc931fff23896933635f79a9862b6e
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2023-08-07 17:16:47 +0900

    refactor: replace code highlighter method from highlightBlock to highlightElement

--- src/components/posts/post-body.tsx
+++ src/components/posts/post-body.tsx
@@ -29,7 +29,7 @@ const PostBody = ({ content }: Props) => {
   useEffect(() => {
     const codeBlocks = document.querySelectorAll("code");
     codeBlocks.forEach((codeBlock) => {
-      hljs.highlightBlock(codeBlock);
+      hljs.highlightElement(codeBlock);
     });
   }, []);
 
