0a94e6c1ce5c8f7c7225de32ed04a649abe205b6
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2023-08-17 13:56:25 +0900

    SEO: add site navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,17 +2,23 @@ import Head from "next/head";
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { addNavigationJsonLd } from "utils/addProductJsonLd";
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
   return (
     <>
       <Head>
+        <title>타이의 쿠키</title>
         <link rel="shortcut icon" href="/favicon.png" />
         <meta
           name="google-site-verification"
           content="DDEZrz9lubJFHFHFDO426TLckWKDClbXLoB4_T3RVk8"
         />
-        <title>타이의 쿠키</title>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={addNavigationJsonLd()}
+          key="product-jsonld"
+        />
       </Head>
       <Navbar />
       <main>{children}</main>
---
+++ src/utils/addNavigationJsonLd.ts
@@ -0,0 +1,33 @@
+import { URL } from "consts/url";
+
+export const addNavigationJsonLd = () => {
+  return {
+    __html: `{
+        "@context": "https://schema.org",
+        "@type": "ItemList",
+        itemListElement: [
+          {
+            '@type': 'SiteNavigationElement',
+            position: 1,
+            name: 'Home',
+            description: 'Tai is a software engineer based in Seoul.',
+            url: ${URL},
+          },
+          {
+            '@type': 'SiteNavigationElement',
+            position: 2,
+            name: 'Posts',
+            description: 'Posts about software engineering, life, and more.',
+            url: '${URL}/posts',
+          },
+          {
+            '@type': 'SiteNavigationElement',
+            position: 3,
+            name: 'Portfolio',
+            description: 'The list of projects Tai has worked on.',
+            url: '${URL}/portfolio',
+          }
+        ],
+      }`,
+  };
+};
