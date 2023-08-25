6545641301a451bfb243062467e7dba2e20ff8b4
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2023-08-25 16:59:19 +0900

    fix: add quotes for crawlers to parse

--- src/utils/addNavigationJsonLd.ts
+++ src/utils/addNavigationJsonLd.ts
@@ -5,27 +5,27 @@ export const addNavigationJsonLd = () => {
     __html: `{
         "@context": "https://schema.org",
         "@type": "ItemList",
-        itemListElement: [
+        "itemListElement": [
           {
-            '@type': 'SiteNavigationElement',
-            position: 1,
-            name: 'Home',
-            description: 'Tai is a software engineer based in Seoul.',
-            url: ${URL},
+            "@type": "SiteNavigationElement",
+            "position": 1,
+            "name": "Home",
+            "description": "Tai is a software engineer based in Seoul.",
+            "url": ${URL},
           },
           {
-            '@type': 'SiteNavigationElement',
-            position: 2,
-            name: 'Posts',
-            description: 'Posts about software engineering, life, and more.',
-            url: '${URL}/posts',
+            "@type": "SiteNavigationElement",
+            "position": 2,
+            "name": "Posts",
+            "description": "Posts about software engineering, life, and more.",
+            "url": "${URL}/posts",
           },
           {
-            '@type': 'SiteNavigationElement',
-            position: 3,
-            name: 'Works',
-            description: 'The list of projects Tai has worked on.',
-            url: '${URL}/works',
+            "@type": "SiteNavigationElement",
+            "position": 3,
+            "name": "Works",
+            "description": "The list of projects Tai has worked on.",
+            "url": "${URL}/works",
           }
         ],
       }`,
