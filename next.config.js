2ae4d7b9bd999db6706b252d97ca7a45e2e19fe6
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2023-09-18 16:01:39 +0900

    config: add new hostname for fetching notion image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["github.com", "s3.us-west-2.amazonaws.com"],
+    domains: ["github.com", "s3.us-west-2.amazonaws.com", "prod-files-secure.s3.us-west-2.amazonaws.com"],
     formats: ["image/avif", "image/webp"],
   },
 };
