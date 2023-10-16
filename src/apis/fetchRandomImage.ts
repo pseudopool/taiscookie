95578c97d110a1596abb2f181ad604211438bad9
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2023-10-16 16:13:51 +0900

    feature: edit random image keyword

--- src/apis/fetchRandomImage.ts
+++ src/apis/fetchRandomImage.ts
@@ -1,7 +1,7 @@
 const fetchRandomImage = async (count = 1) => {
   return await (
     await fetch(
-      `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&query=3d&count=${count}&orientation=landscape`
+      `https://api.unsplash.com/photos/random?client_id=${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}&query=cute,3d&count=${count}&orientation=landscape`
     )
   ).json();
 };
