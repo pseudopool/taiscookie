59478787b6f1960464b7613f42a4855dd7fdfaf8
tai song <pseudopool@gmail.com>
2024-02-16 19:51:55 +0900

    feat: add main image to root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <section>
-        <h1>Tai's Cookie</h1>
+      <section className="relative">
+        <Image
+          src="/main.jpeg"
+          alt="Tai seating on a cloud with a laptop and two dogs."
+          width={200}
+          height={200}
+          className="rounded"
+        />
       </section>
     </main>
   );
