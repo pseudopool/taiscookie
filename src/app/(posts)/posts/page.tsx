b9cb12d04ce4891c01a2f7a902642cbe2efdb451
tai song <pseudopool@gmail.com>
2024-02-28 15:37:53 +0900

    feature: add relative time to post list

--- src/app/(posts)/posts/page.tsx
+++ src/app/(posts)/posts/page.tsx
@@ -4,6 +4,30 @@ import { fetchPosts } from "@/apis/fetchPosts";
 import { formatNotionPost } from "@/libs/formatNotionPost";
 import { Post } from "@/types/post";
 
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
+
+dayjs.extend(relativeTime);
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s 전",
+    s: "1초",
+    m: "1분",
+    mm: "%d분",
+    h: "1시간",
+    hh: "%d시간",
+    d: "1일",
+    dd: "%d일",
+    M: "1달",
+    MM: "%d달",
+    y: "1년",
+    yy: "%d년",
+  },
+});
+
 const PostsPage = async () => {
   const allPosts = await fetchPosts().then((res) =>
     res.results.map((post: any) => formatNotionPost(post))
@@ -23,6 +47,7 @@ const PostsPage = async () => {
           <div className="p-4 font-mono break-keep flex flex-col gap-4">
             <h2 className="text-xl font-bold">{post.title}</h2>
             <p className="text-gray-600">{post.excerpt}</p>
+            <span>{dayjs(post.date).fromNow()}</span>
           </div>
         </li>
       ))}
