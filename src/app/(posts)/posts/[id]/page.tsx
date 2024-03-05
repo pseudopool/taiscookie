0a0023f6aa3be37106f8892c5d6af71138da020f
tai song <pseudopool@gmail.com>
2024-03-05 15:21:25 +0900

    feature: add suspense to post details

--- src/app/(posts)/posts/[id]/page.tsx
+++ src/app/(posts)/posts/[id]/page.tsx
@@ -1,17 +1,13 @@
-import { fetchPost } from "@/apis/fetchPost";
-import PostBody from "@/components/post/body";
+import Loading from "@/components/loading";
+import PostDetail from "@/components/post/detail";
+import { Suspense } from "react";
 
-const PostPage = async ({ params }: { params: { id: string } }) => {
-  const post = await fetchPost(params.id);
-
-  return (
-    <section className="p-8 w-full flex flex-col gap-4">
-      <h1 className="text-2xl text-center font-bold">{post.title}</h1>
-      <h2 className="text-xl text-center font-medium">{post.excerpt}</h2>
-      <span className="block text-lg text-center font-thin">{post.date}</span>
-      <PostBody content={post.content} />
-    </section>
-  );
-};
+const PostPage = async ({ params }: { params: { id: string } }) => (
+  <section className="p-8 w-full flex flex-col gap-4">
+    <Suspense fallback={<Loading />}>
+      <PostDetail id={params.id} />
+    </Suspense>
+  </section>
+);
 
 export default PostPage;
---
+++ src/components/post/detail.tsx
@@ -0,0 +1,21 @@
+import { fetchPost } from "@/apis/fetchPost";
+import PostBody from "./body";
+
+type Props = {
+  id: string;
+};
+
+const PostDetail = async ({ id }: Props) => {
+  const post = await fetchPost(id);
+
+  return (
+    <>
+      <h1 className="text-2xl text-center font-bold">{post.title}</h1>
+      <h2 className="text-xl text-center font-medium">{post.excerpt}</h2>
+      <span className="block text-lg text-center font-thin">{post.date}</span>
+      <PostBody content={post.content} />
+    </>
+  );
+};
+
+export default PostDetail;
