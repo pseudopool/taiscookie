30ddf4e09737fc74efa00e3e3cf83d78bcfdeb71
tai song <pseudopool@gmail.com>
2024-03-05 15:29:01 +0900

    feature: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import Navigator from "@/components/navigator";
+import Image from "next/image";
+import Link from "next/link";
 
 const noto_sans_kr = Noto_Sans_KR({ subsets: ["latin"] });
 
@@ -20,6 +22,11 @@ export default function RootLayout({
       <body className={noto_sans_kr.className}>
         <Navigator />
         {children}
+        <footer className="w-full flex flex-col items-center justify-center pt-8 pb-12">
+          <Link href="https://github.com/pseudopool" target="_blank">
+            <Image src="/github.svg" alt="github" width={40} height={40} />
+          </Link>
+        </footer>
       </body>
     </html>
   );
