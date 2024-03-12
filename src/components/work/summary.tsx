b6cc6418070b215a1242e9f40f109f5a29b089aa
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2024-03-12 14:41:03 +0900

    feature: add work experiences

---
+++ src/components/work/summary.tsx
@@ -0,0 +1,24 @@
+import { Job } from "@/consts/works";
+import Image from "next/image";
+import Link from "next/link";
+
+type Props = {
+  job: Job;
+};
+
+const Summary = ({ job }: Props) => (
+  <div className="flex justify-center items-center gap-3 p-6">
+    <Image src="/ktown4u.ico" alt="ktown4u" width={80} height={80} />
+    <div className="flex h-full flex-col gap-1 justify-between">
+      <h2 className="text-xl font-bold">{job.name}</h2>
+      <h3 className="text-gray-500 mb-1 text-sm">{job.role}</h3>
+      <Link href="https://www.ktown4u.com" target="_blank">
+        <span className="bg-gray-100 text-sm px-2 py-1 rounded-lg text-gray-500">
+          {job.link}
+        </span>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Summary;
--- src/components/work/works.tsx
+++ src/components/work/works.tsx
@@ -2,11 +2,20 @@
 
 import { useState } from "react";
 import ToggleGroup from "../toggle-group";
-import { works } from "@/consts/works";
+import { Job } from "@/consts/works";
 
 const skills = ["Features", "UI/UX", "Performance"];
 
-const Works = () => {
+type Props = {
+  works: Job["works"];
+};
+
+type Work = {
+  title: string;
+  contents: string[];
+};
+
+const Works = ({ works }: Props) => {
   const [active, setActive] = useState(skills[0]);
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) =>
@@ -16,7 +25,7 @@ const Works = () => {
     <>
       <ToggleGroup groups={skills} handleClick={handleClick} active={active} />
       <ul className="w-full py-6 flex flex-col gap-6">
-        {works[active as keyof typeof works].map((work) => (
+        {works[active as keyof typeof works].map((work: Work) => (
           <li key={work.title} className="flex flex-col gap-4">
             <h3 className="text-lg font-mono font-semibold border-b pb-2 text-gray-700">
               {work.title}
