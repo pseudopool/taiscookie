bc6985c1f51998b95caab4d42249c5dc80d5759f
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2024-03-19 11:48:54 +0900

    feature: create image index logic

--- src/components/post/posts.tsx
+++ src/components/post/posts.tsx
@@ -11,7 +11,7 @@ const Posts = async () => {
   return (
     <ul className="p-6 w-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
       {allPosts.map((post: PostType, index: number) => (
-        <Post key={post.id} post={post} index={index} />
+        <Post key={post.id} post={post} index={allPosts.length - index - 1} />
       ))}
     </ul>
   );
