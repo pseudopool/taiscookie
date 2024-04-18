cebe40b25d5418d7fe47df7be5a498759bd5476d
송송이 <songsong-i@songsong-iui-MacBookPro.local>
2024-04-18 14:16:30 +0900

    fix: adjust same revalidate policy for post

--- src/apis/fetchPosts.ts
+++ src/apis/fetchPosts.ts
@@ -3,9 +3,7 @@ export const fetchPosts = async () =>
     await fetch(
       `https://api.notion.com/v1/databases/${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}/query`,
       {
-        next: {
-          revalidate: 3600,
-        },
+        cache: "no-cache",
         headers: {
           Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTION_API_KEY}`,
           "Notion-Version": "2022-06-28",
