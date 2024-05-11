432c97a0d6faa6de6bc2b5de645a4a20e2b5dcf6
tai song <98731086+pseudopool@users.noreply.github.com>
2024-05-11 16:46:53 +0900

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://taiscookie.vercel.app",
+      url: "https://taiscookie.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
     },
     {
-      url: "https://taiscookie.vercel.app/posts",
+      url: "https://taiscookie.com/posts",
       lastModified: new Date(),
       changeFrequency: "hourly",
       priority: 0.9,
     },
     {
-      url: "https://taiscookie.vercel.app/works",
+      url: "https://taiscookie.com/works",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
     {
-      url: "https://taiscookie.vercel.app/etc",
+      url: "https://taiscookie.com/etc",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.5,
