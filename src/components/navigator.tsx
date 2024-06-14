099f6f6403bdf70a8c07e76f36042f2832b2e011
tai song <pseudopool@gmail.com>
2024-06-14 18:32:39 +0900

    style: change navigator font

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navigator = () => {
   return (
-    <nav className="p-2 sticky top-0 bg-white border-b-2 border-black z-10">
+    <nav className="p-2 sticky top-0 bg-white border-b-2 border-black z-10 font-azeret">
       <ul className="flex text-lg w-full items-center justify-around font-semibold">
         <li>
           <Link href="/posts">Posts</Link>
