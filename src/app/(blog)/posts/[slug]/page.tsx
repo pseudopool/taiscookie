dc09ed2dcc2be4fdbd5fc6bbcfcb435c598a23a3
송송이 <pseudopool@gmail.com>
2025-09-22 16:43:08 +0900

    feature: 포스트 상세 sanity 연동

--- src/app/(blog)/posts/[slug]/page.tsx
+++ src/app/(blog)/posts/[slug]/page.tsx
@@ -1,13 +1,74 @@
+import { Suspense } from "react";
+import type { Metadata, ResolvingMetadata } from "next";
+
 import Loading from "@/components/loading";
 import PostDetail from "@/components/post/detail";
-import { Suspense } from "react";
+import { sanityFetch } from "@/libs/sanity/live";
+import { postPagesSlugs, postQuery } from "@/libs/sanity/queries";
+import { resolveOpenGraphImage } from "@/libs/sanity/utils";
 
-const PostPage = async ({ params }: { params: { id: string } }) => (
-  <section className="pb-8 flex flex-col max-w-screen-2xl w-full mx-auto">
-    <Suspense fallback={<Loading />}>
-      <PostDetail id={params.id} />
-    </Suspense>
-  </section>
-);
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+/**
+ * Generate the static params for the page.
+ * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+ */
+export async function generateStaticParams() {
+  const { data } = await sanityFetch({
+    query: postPagesSlugs,
+    // Use the published perspective in generateStaticParams
+    perspective: "published",
+    stega: false,
+  });
+  return data;
+}
+
+/**
+ * Generate metadata for the page.
+ * Learn more: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#generatemetadata-function
+ */
+export async function generateMetadata(
+  props: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const params = await props.params;
+  const { data: post } = await sanityFetch({
+    query: postQuery,
+    params,
+    // Metadata should never contain stega
+    stega: false,
+  });
+  const previousImages = (await parent).openGraph?.images || [];
+  const ogImage = resolveOpenGraphImage(post?.coverImage);
+
+  return {
+    authors:
+      post?.author?.firstName && post?.author?.lastName
+        ? [{ name: `${post.author.firstName} ${post.author.lastName}` }]
+        : [],
+    title: post?.title,
+    description: post?.excerpt,
+    openGraph: {
+      images: ogImage ? [ogImage, ...previousImages] : previousImages,
+    },
+  } satisfies Metadata;
+}
+
+const PostPage = async (props: Props) => {
+  const params = await props.params;
+  const [{ data: post }] = await Promise.all([
+    sanityFetch({ query: postQuery, params }),
+  ]);
+
+  return (
+    <section className="pb-8 flex flex-col max-w-screen-2xl w-full mx-auto">
+      <Suspense fallback={<Loading />}>
+        <PostDetail post={post} />
+      </Suspense>
+    </section>
+  );
+};
 
 export default PostPage;
