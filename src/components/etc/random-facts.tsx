d21949c79e61018f3c18f339ee72b681e2f5e667
송송이 <pseudopool@gmail.com>
2025-09-24 17:21:11 +0900

    packages: update packages for three.js

--- src/components/etc/random-facts.tsx
+++ src/components/etc/random-facts.tsx
@@ -1,62 +1,61 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { facts } from "@/consts/facts";
 import Board from "../models/Board";
 import FortuneCookie from "../models/FortuneCookie";
 
-import { facts } from "@/consts/facts";
-import { useEffect, useState } from "react";
-
 const RandomFacts = () => {
-  const [randomFact, setRandomFact] = useState("");
-  const [shownFacts, setShownFacts] = useState([randomFact]);
+	const [randomFact, setRandomFact] = useState("");
+	const [shownFacts, setShownFacts] = useState([randomFact]);
 
-  const handleClick = () => {
-    const newFact = facts[Math.floor(Math.random() * facts.length)];
-    if (shownFacts.length === facts.length) {
-      setShownFacts([newFact]);
-      setRandomFact(newFact);
-      return;
-    }
-    if (shownFacts.includes(newFact)) {
-      handleClick();
-      return;
-    }
-    setShownFacts([...shownFacts, newFact]);
-    setRandomFact(newFact);
-  };
+	const handleClick = () => {
+		const newFact = facts[Math.floor(Math.random() * facts.length)];
+		if (shownFacts.length === facts.length) {
+			setShownFacts([newFact]);
+			setRandomFact(newFact);
+			return;
+		}
+		if (shownFacts.includes(newFact)) {
+			handleClick();
+			return;
+		}
+		setShownFacts([...shownFacts, newFact]);
+		setRandomFact(newFact);
+	};
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setRandomFact("");
-    }, 1500);
-    return () => clearTimeout(timeout);
-  }, [randomFact]);
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setRandomFact("");
+		}, 1500);
+		return () => clearTimeout(timeout);
+	}, [randomFact]);
 
-  return (
-    <>
-      <p className="font-azeret text-center px-6 break-keep text-slate-400">
-        Click and see what you get.
-      </p>
-      <button onClick={handleClick} className="w-full">
-        <Board>
-          <FortuneCookie />
-        </Board>
-      </button>
-      <p
-        className="absolute mx-4 w-fit rotate-6 max-w-screen-sm font-azeret text-center px-2 py-1 bg-gray-100 break-keep transition-opacity duration-300 select-none"
-        style={{
-          opacity: randomFact ? 1 : 0,
-          backgroundColor: randomFact
-            ? "rgba(255, 255, 255, 0.9)"
-            : "transparent",
-          border: randomFact
-            ? "1px solid rgba(0, 0, 0)"
-            : "1px solid transparent",
-        }}
-      >
-        {randomFact}
-      </p>
-    </>
-  );
+	return (
+		<>
+			<p className="font-azeret text-center px-6 break-keep text-slate-400">
+				Click and see what you get.
+			</p>
+			<button type="button" onClick={handleClick} className="w-full">
+					<Board>
+						<FortuneCookie />
+					</Board>
+			</button>
+			<p
+				className="absolute mx-4 w-fit rotate-6 max-w-screen-sm font-azeret text-center px-2 py-1 bg-gray-100 break-keep transition-opacity duration-300 select-none"
+				style={{
+					opacity: randomFact ? 1 : 0,
+					backgroundColor: randomFact
+						? "rgba(255, 255, 255, 0.9)"
+						: "transparent",
+					border: randomFact
+						? "1px solid rgba(0, 0, 0)"
+						: "1px solid transparent",
+				}}
+			>
+				{randomFact}
+			</p>
+		</>
+	);
 };
 export default RandomFacts;
--- src/components/models/Board.tsx
+++ src/components/models/Board.tsx
@@ -2,39 +2,29 @@
 
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { ReactNode, Suspense } from "react";
-import { MoonLoader } from "react-spinners";
+import type { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 const Board = ({ children }: Props) => {
-  return (
-    <div
-      style={{
-        height: "60vh",
-        maxHeight: "1200px",
-      }}
-      className="w-full"
-      id="container"
-    >
-      <Suspense
-        fallback={
-          <div className="w-full h-full flex justify-center items-center">
-            <MoonLoader size={40} />
-          </div>
-        }
-      >
-        <Canvas>
-          <OrbitControls autoRotate autoRotateSpeed={20} />
-          <ambientLight intensity={5} />
-          <group>{children}</group>
-          <PerspectiveCamera makeDefault position={[20, 90, 80]} />
-        </Canvas>
-      </Suspense>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				height: "60vh",
+				maxHeight: "1200px",
+			}}
+			className="w-full"
+		>
+			<Canvas>
+				<OrbitControls autoRotate autoRotateSpeed={20} />
+				<ambientLight intensity={5} />
+				<group>{children}</group>
+				<PerspectiveCamera makeDefault position={[20, 90, 80]} />
+			</Canvas>
+		</div>
+	);
 };
 
 export default Board;
