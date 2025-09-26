e6e8f80f862821c07a34a848b2095310a63c1015
송송이 <pseudopool@gmail.com>
2025-09-26 15:47:07 +0900

    fix: remove waitForContentLake option from sanity revalidate

--- src/app/api/revalidate/tag/route.ts
+++ src/app/api/revalidate/tag/route.ts
@@ -18,7 +18,6 @@ export async function POST(req: NextRequest) {
 		const { isValidSignature, body } = await parseBody<WebhookPayload>(
 			req,
 			process.env.SANITY_REVALIDATE_SECRET,
-			true,
 		);
 
 		if (!isValidSignature) {
